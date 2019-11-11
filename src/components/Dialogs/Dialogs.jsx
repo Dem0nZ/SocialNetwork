ccc000d329aa745292bdbdb438bae3f3bdbaee2a
Dem0nz <cbpskov@gmail.com>
2019-11-12 01:17:52 +0300

    add work with Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,9 +2,41 @@ import React from 'react';
 import classes from './Dialogs.module.css';
 
 const Dialogs = () => {
-    return <div>
-        dialogs
-    </div>;
+    return (
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    MaxPes
+                </div>
+                <div className={classes.dialog}>
+                    Pavelexus
+                </div>
+                <div className={classes.dialog}>
+                    Kalem
+                </div>
+                <div className={classes.dialog}>
+                    Sanek
+                </div>
+                <div className={classes.dialog}>
+                    Luxur
+                </div>
+                <div className={classes.dialog}>
+                    BatulinS
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>
+                    Hi!
+                </div>
+                <div className={classes.message}>
+                    How Are you?
+                </div>
+                <div className={classes.message}>
+                    DHDHDHD!!!
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Dialogs;
