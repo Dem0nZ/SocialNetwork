381cdedb3b93f98494b53c3bdbfbf6fa7cfc33af
Dem0nz <cbpskov@gmail.com>
2019-11-18 04:36:13 +0300

    slice BLL&UI

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,31 +4,8 @@ import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 
 const Dialogs = (props) => {
-
-    let dialogs = [
-        {id: 1, name: 'MaxPes'},
-        {id: 2, name: 'Pavelexus'},
-        {id: 3, name: 'Kalem'},
-        {id: 4, name: 'Sanek'},
-        {id: 5, name: 'Luxur'},
-        {id: 6, name: 'BatulinS'},
-    ]
-
-
-    let dialogsElements = dialogs.map( d => <DialogItem name={d.name} id={d.id}/> );
-
-
-    let messages = [
-        {id: 1, message: 'Hi.'},
-        {id: 2, message: 'How are you?'},
-        {id: 3, message: 'pepegaaaa'},
-        {id: 4, message: 'I don\'t know'},
-        {id: 5, message: 'YoYo'},
-        {id: 6, message: 'psina lohmataya'},
-    ]
-
-    let messagesElements = messages.map( m => <Message message={m.message}/> );
-
+    let dialogsElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messagesElements = props.messages.map(m => <Message message={m.message}/>);
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,14 +4,7 @@ import Post from './Post/Post';
 
 const MyPosts = (props) => {
 
-    let posts = [
-        {id: 1, message: 'Hi, how are you?', likeCounts: '14'},
-        {id: 2, message: 'It\'s my first post', likeCounts: '2'},
-        {id: 3, message: 'pepegaaaa', likeCounts: '18'},
-        {id: 4, message: 'Maks pes', likeCounts: '99'}
-    ]
-
-    let postElements = posts.map(p => <Post message={p.message} likeCounts={p.likeCounts}/>);
+    let postElements = props.posts.map(p => <Post message={p.message} likeCounts={p.likeCounts}/>);
 
     return <div className={classes.postBlock}>
         <h2>My posts</h2>
