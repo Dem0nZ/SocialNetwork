929717efb7c8e1881ec0d1d334f0005fb7e5147b
Dem0nZ <cbpskov@gmail.com>
2019-11-29 23:19:39 +0300

    Revert "Complete merge"
    This reverts commit fcf47e08

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,8 +5,23 @@ import Message from "./Message/Message";
 
 const Dialogs = (props) => {
     let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
-    let messagesElements = props.state.messages.map(m => <Message message={m.message}/>);
-    return (
+    let messagesElements = props.state.messages.map(m => <Message message={m.message} isCurrentUser={m.userId === props.state.userId}/>);
+debugger
+    let textMessage = React.createRef();
+
+    let addMessage = () => {
+        if (textMessage.current.value){
+            props.addMessage();
+        }
+
+    }
+
+    let onMessageChange =() => {
+        let text = textMessage.current.value;
+        props.updateNewMessageText(text);
+    }
+
+    return (<div>
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
                 {dialogsElements}
@@ -15,7 +30,12 @@ const Dialogs = (props) => {
                 {messagesElements}
             </div>
         </div>
+            <div className={classes.addMessage}>
+                <textarea ref={textMessage} value={props.state.newDialogText} onChange={onMessageChange}/>
+                <button onClick={addMessage} >Message</button>
+            </div>
+        </div>
     )
-};
+}
 
 export default Dialogs;
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,7 @@
+let rerenderEntireTree =() => {
+    console.log('state was changet')
+}
+
 let state = {
     profilePage: {
         posts: [
@@ -5,16 +9,19 @@ let state = {
             {id: 2, message: 'It\'s my first post', likeCounts: '2'},
             {id: 3, message: 'pepegaaaa', likeCounts: '18'},
             {id: 4, message: 'Maks pes', likeCounts: '99'}
-        ]
+        ],
+        newPostText: ''
     },
     dialogsPage: {
+        userId: 1,
         messages: [
-            {id: 1, message: 'Hi.'},
-            {id: 2, message: 'How are you?'},
-            {id: 3, message: 'pepegaaaa'},
-            {id: 4, message: 'I don\'t know'},
-            {id: 5, message: 'YoYo'},
-            {id: 6, message: 'psina lohmataya'}
+            {id: 1, userId: 1, message: 'Hi.'},
+            {id: 2, userId: 2, message: 'How are you?'},
+            {id: 3, userId: 1, message: 'pepegaaaa'},
+            {id: 4, userId: 1, message: 'I don\'t know'},
+            {id: 5, userId: 2, message: 'YoYo'},
+            {id: 6, userId: 2, message: 'psina lohmataya'}
+
         ],
         dialogs: [
             {id: 1, name: 'MaxPes'},
@@ -23,8 +30,47 @@ let state = {
             {id: 4, name: 'Sanek'},
             {id: 5, name: 'Luxur'},
             {id: 6, name: 'BatulinS'}
-        ]
+        ],
+        newDialogText: ''
     }
-};
+
+}
+
+export const addPost = () =>{
+    let newPost = {
+        id: 5,
+        message: state.profilePage.newPostText,
+        likeCounts: 0
+    };
+    state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = '';
+    rerenderEntireTree(state);
+}
+
+export const updateNewPostText = (newText) =>{
+    state.profilePage.newPostText = newText;
+    rerenderEntireTree(state);
+}
+
+export const addMessage = () =>{
+    let newMessage = {
+        id: 7,
+        message: state.dialogsPage.newDialogText,
+        userId: 1
+    };
+    state.dialogsPage.messages.push(newMessage);
+    state.dialogsPage.newDialogText = '';
+    debugger;
+    rerenderEntireTree(state);
+}
+
+export const updateNewMessageText = (newMess) => {
+    state.dialogsPage.newDialogText = newMess;
+    rerenderEntireTree(state);
+}
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;   //observer
+}
 
 export default state;
