fcf47e08c5c74de5ca5bac564ba0c1b83f747234
Dem0nZ <cbpskov@gmail.com>
2019-11-29 22:12:37 +0300

    Complete merge

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import classes from './../Dialogs.module.css';
 
 const Message = (props) => {
-    return <div className={props.isCurrentUser ? classes.myDialog : classes.dialog}>
-        <div><img src='https://liveopencart.ru/image/cache/data/products/coverreview-400x400.jpg'></img></div>
-        <div>{props.message}</div>
-    </div>
-}
-
+    return <div className={classes.dialog}>{props.message}</div>
+};
 
 export default Message;
