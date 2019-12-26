79d03745b54e439b079f0f9047f2ed8d9a4235db
Dem0nZ <cbpskov@gmail.com>
2019-12-26 19:32:19 +0300

    reducwe fix$ dialogs

--- src/components/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './../Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
+
 const DialogItem = (props) => {
 
     return <div className={classes.chats + ' ' + classes.active}>
