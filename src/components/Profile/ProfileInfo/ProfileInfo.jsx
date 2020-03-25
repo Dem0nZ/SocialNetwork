08cbbc242653fddbe10be4f1cf028e559b7d8e65
Dem0nZ <cbpskov@gmail.com>
2020-03-25 20:23:29 +0300

    make some lines in Profile info

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -14,6 +14,9 @@ const ProfileInfo = (props) => {
             </div>
             <div className={classes.descriptionBlock}>
                 <img alt='' src={props.profile.photos.large}/>
+                <div>Полное имя: {props.profile.fullName}</div>
+                <div>Обо мне:{props.profile.aboutMe}</div>
+                <div>Ищу работу: {props.profile.lookingForAJob?'да':'нет'}</div>
             </div>
         </div>
     )
